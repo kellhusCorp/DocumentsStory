8b20fd199ff1221e886dc2f0c2de81216077b554
kellhusCorp <alkan_7@mail.ru>
2023-02-15 20:04:52 +0400

    Добавил скрипт добавления документов.

---
+++ SqlQueries/InsertTestDocuments.sql
@@ -0,0 +1,28 @@
+﻿USE [DocumentsDb]
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'8bc88867-49ad-ed11-b19c-14dda953d821', 102, CAST(N'2002-10-10T00:00:00.000' AS DateTime), CAST(N'2012-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'8cc88867-49ad-ed11-b19c-14dda953d821', 103, CAST(N'2003-10-10T00:00:00.000' AS DateTime), CAST(N'2013-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'8dc88867-49ad-ed11-b19c-14dda953d821', 104, CAST(N'2004-10-10T00:00:00.000' AS DateTime), CAST(N'2014-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'8ec88867-49ad-ed11-b19c-14dda953d821', 105, CAST(N'2005-10-10T00:00:00.000' AS DateTime), CAST(N'2015-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'8fc88867-49ad-ed11-b19c-14dda953d821', 106, CAST(N'2006-10-10T00:00:00.000' AS DateTime), CAST(N'2016-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'90c88867-49ad-ed11-b19c-14dda953d821', 107, CAST(N'2007-10-10T00:00:00.000' AS DateTime), CAST(N'2017-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'91c88867-49ad-ed11-b19c-14dda953d821', 108, CAST(N'2008-10-10T00:00:00.000' AS DateTime), CAST(N'2018-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'92c88867-49ad-ed11-b19c-14dda953d821', 109, CAST(N'2009-10-10T00:00:00.000' AS DateTime), CAST(N'2019-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'93c88867-49ad-ed11-b19c-14dda953d821', 110, CAST(N'2010-10-10T00:00:00.000' AS DateTime), CAST(N'2020-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'94c88867-49ad-ed11-b19c-14dda953d821', 111, CAST(N'2011-10-10T00:00:00.000' AS DateTime), CAST(N'2021-10-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'95c88867-49ad-ed11-b19c-14dda953d821', 112, CAST(N'2023-01-10T00:00:00.000' AS DateTime), CAST(N'2023-02-11T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'96c88867-49ad-ed11-b19c-14dda953d821', 113, CAST(N'2023-01-11T00:00:00.000' AS DateTime), CAST(N'2023-02-12T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Documents] ([Id], [Number], [CreatedAt], [LastUpdateAt]) VALUES (N'97c88867-49ad-ed11-b19c-14dda953d821', 114, CAST(N'2023-01-12T00:00:00.000' AS DateTime), CAST(N'2023-02-13T00:00:00.000' AS DateTime))
+GO
